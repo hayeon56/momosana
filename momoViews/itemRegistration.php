8e9e79c5dd800fd05f61683e0e80359b71eaa387
momo <hayoen56>
2019-07-30 18:12:53 +0900

    sully

--- momoViews/itemRegistration.php
+++ momoViews/itemRegistration.php
@@ -138,7 +138,7 @@ function image_delete(){
     <h1>商品登録</h1>
     <div class="my-box">
     <h2>基本情報</h2>
-    <form name="momoform" action="momo/momoControllers/momoController.php/itemRegistration" onsubmit="return test()" method="post" id="sou">
+    <form name="momoform" action="../momoControllers/momoController.php/itemRegistration" onsubmit="return test()" method="post" id="sou">
     <strong>商品番号</strong><br><br><input type="text" name="item_code" placeholder="商品番号" >
     <p>※半角英数字「-（ハイフン）」、「_（アンダーバー）」64文字以内</p>
     <hr>
